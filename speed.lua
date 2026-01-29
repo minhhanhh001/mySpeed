484dde93a7d4ec64cf7af18659889ed9c50e5684
minhhanhh001 <hoangminh31101995@gmail.com>
2026-01-30 01:30:54 +0700

    Update speed.lua

--- speed.lua
+++ speed.lua
@@ -2,16 +2,20 @@ local players = game:GetService("Players")
 
 local function bypassWalkSpeed()
     if getgenv().executed then
-        if not getgenv().Enabled then return end
+        print("Walkspeed Already Bypassed - Applying Settings Changes")
+        if not getgenv().Enabled then
+            return
+        end
     else
         getgenv().executed = true
+        print("Walkspeed Bypassed")
 
         local mt = getrawmetatable(game)
         setreadonly(mt, false)
 
         local oldindex = mt.__index
         mt.__index = newcclosure(function(self, b)
-            if b == "WalkSpeed" then
+            if b == 'WalkSpeed' then
                 return 16
             end
             return oldindex(self, b)
@@ -26,14 +30,7 @@ players.LocalPlayer.CharacterAdded:Connect(function(char)
     char:WaitForChild("Humanoid").WalkSpeed = getgenv().Speed
 end)
 
-local lastSpeed = nil
-
 while getgenv().Enabled and wait() do
-    local hum = players.LocalPlayer.Character:WaitForChild("Humanoid")
-    if lastSpeed ~= getgenv().Speed then
-        hum.WalkSpeed = getgenv().Speed
-        lastSpeed = getgenv().Speed
-    end
+    players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = getgenv().Speed
 end
-
 
